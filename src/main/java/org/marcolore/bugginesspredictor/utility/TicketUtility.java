64e5c1537a28ae2140bbd0e69ee81fbcb233dbaa
MarcoLor01 <marcolorenzini23@gmail.com>
2024-10-05 16:11:52 +0200

    0 code smells

--- src/main/java/org/marcolore/bugginesspredictor/utility/TicketUtility.java
+++ src/main/java/org/marcolore/bugginesspredictor/utility/TicketUtility.java
@@ -2,8 +2,6 @@ package org.marcolore.bugginesspredictor.utility;
 
 import org.marcolore.bugginesspredictor.model.Release;
 import org.marcolore.bugginesspredictor.model.Ticket;
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
